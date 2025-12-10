0586154350b36d4bf9ecc8656918954e22582f61
Robert Preston <refinedvalleydude@gmail.com>
2025-12-10 17:13:41 +0000

    Add pytest suite for init_schema.py

---
+++ tests/test_init_schema.py
@@ -0,0 +1,30 @@
+from unittest.mock import Mock
+import pytest
+from crypto_lambda.init_schema import ensure_table_exists
+
+def test_ensure_table_exists_success():
+
+    # Create conn object
+    mock_conn = Mock()
+
+    # Run
+    ensure_table_exists(mock_conn)
+
+    # Assertions
+    mock_conn.run.assert_called_once()
+    args = mock_conn.run.call_args[0][0]
+    assert "CREATE TABLE" in args
+
+def test_ensure_table_exists_failure(capsys):
+
+    # Create conn object and cause exception
+    mock_conn = Mock()
+    mock_conn.run.side_effect = Exception("some failure")
+
+    # Run
+    ensure_table_exists(mock_conn)
+
+    # Assertions
+    out = capsys.readouterr().out
+    assert "Failed to ensure table exists" in out
+    assert "some failure" in out
